6758b0e6031488957a5cd37c732b883c08a08543
tawfiqsiam <toto.siam999@gmail.com>
2018-12-24 20:18:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,11 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} !`);
- 
+client.on('guildMemberAdd', member => {
+      if(member.guild.id !== '477149000987049984') return;
+    setTimeout(function() {
+    member.guild.channels.find(r => r.id === '495636666619396105').send('**Welcome To Star Central .**');
+},3000);
 });
- 
 
-var PrEfix = ".";
-client.on('message', message => {
-  if (!message.content.startsWith(PrEfix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '521479557375852547') return;
-if (message.content.startsWith(PrEfix + 'wt')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(PrEfix + 'ls')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(PrEfix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/amira");
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-}
-
-});
 
 client.login(process.env.BOT_TOKEN);
